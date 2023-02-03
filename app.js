34aa11f62fcd4eb23e0ad8cf362b196dc79d5e9e
dmychel <dmychel@tutanota.com>
2023-02-03 14:59:10 -0500

    created most if not all of the styling, now am onto making the design fit both tablet and desktop

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('#year').forEach(link => {
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+    }
+})
+console.log(navLinks)
